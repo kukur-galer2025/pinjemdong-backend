b11a62e9cb165af60cd8375fc0fd00ba444bf132
kukur dell <kukurdell@gmail.com>
2026-06-10 18:52:52 +0700

    Allow pinjemdong.my.id origin for CORS

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001'],
+    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001', 'https://pinjemdong.my.id'],
 
     'allowed_origins_patterns' => [],
 
